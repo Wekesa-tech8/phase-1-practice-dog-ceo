d7442970a443b19e39dcc60e76565ee7d576bf6d
David Wekesa <wekesadavid300@gmail.com>
2024-07-16 23:42:32 +0300

    Dog

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    const breedUrl = "https://dog.ceo/api/breeds/list/all";
+    const imageContainer = document.getElementById('dog-image-container');
+    const breedList = document.getElementById('dog-breeds');
+    const breedDropdown = document.getElementById('breed-dropdown');
+  
+    fetch(imgUrl)
+      .then(response => response.json())
+      .then(data => {
+        data.message.forEach(imageUrl => {
+          const img = document.createElement('img');
+          img.src = imageUrl;
+          imageContainer.appendChild(img);
+        });
+      });
+  
+    fetch(breedUrl)
+      .then(response => response.json())
+      .then(data => {
+        const breeds = data.message;
+        for (const breed in breeds) {
+          const li = document.createElement('li');
+          li.textContent = breed;
+          breedList.appendChild(li);
+
+          li.addEventListener('click', () => {
+            li.style.color = 'blue'; 
+          });
+        }
+      });
+    breedDropdown.addEventListener('change', (event) => {
+      const selectedLetter = event.target.value;
+      const allBreeds = breedList.getElementsByTagName('li');
+  
+      for (let i = 0; i < allBreeds.length; i++) {
+        const breed = allBreeds[i];
+        if (selectedLetter === 'all' || breed.textContent.startsWith(selectedLetter)) {
+          breed.style.display = '';
+        } else {
+          breed.style.display = 'none';
+        }
+      }
+    });
+  });
